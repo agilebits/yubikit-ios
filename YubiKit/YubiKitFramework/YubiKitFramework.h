39e7e315e3579cb6e661b613801386f5332ff796
Rudy Richter <rarichter@gmail.com>
2019-06-27 21:12:06 -0400

    Adds a Framework build target to the project

---
+++ YubiKit/YubiKitFramework/YubiKitFramework.h
@@ -0,0 +1,19 @@
+//
+//  YubiKitFramework.h
+//  YubiKitFramework
+//
+//  Created by rudy on 6/27/19.
+//  Copyright © 2019 Yubico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YubiKitFramework.
+FOUNDATION_EXPORT double YubiKitFrameworkVersionNumber;
+
+//! Project version string for YubiKitFramework.
+FOUNDATION_EXPORT const unsigned char YubiKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YubiKitFramework/PublicHeader.h>
+
+
